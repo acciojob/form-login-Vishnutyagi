d7c10727a9effd2d26cced10d567c74c0c386c63
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-05-25 00:05:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+var data=document.querySelectorAll(input);
+data[2].addEventListener("click",getFormvalue);
 
+function getFormvalue() {
+	var form = document.getElementById("form1");
+    var firstName = form.fname.value.trim();
+    var lastName = form.lname.value.trim();
+    var fullName = firstName + " " + lastName;
+    alert(fullName);
 }
